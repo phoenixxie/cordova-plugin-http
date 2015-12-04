ce34caf42aa775dcdb2ca6c1adf70a9329aecf32
Kun Xie <phoenix.xie@gmail.com>
2015-12-04 13:06:18 -0500

    fix import

--- src/android/com/synconset/CordovaHTTP/CordovaHttp.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttp.java
@@ -13,6 +13,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.Map;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import java.net.MalformedURLException;
